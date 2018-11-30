7d71daaec0f112ede5fe7f5f9ca28323331f05fd
Adjoa <adjoa.darien@gmail.com>
2018-11-30 14:05:55 +0000

    use ffaker gem to create sample user in factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,14 @@
+require "ffaker"
+
 FactoryBot.define do
   factory :user do
     email { "test@example.com" }
     password { "f4k3p455w0rd" }
+    
+    
+    factory :sample_user do
+      email { FFaker::Internet.email }
+      password { "password" }
+    end
   end
 end
